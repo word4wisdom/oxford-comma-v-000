088138384e929f4d8c723a465217da7e209dc078
Joan' Araujo <wisdombthegame@gmail.com>
2017-11-10 00:09:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.count == 1
+return array.join
+elsif  array.count == 2
+  array.join(" and ")
+else
+  last_index = array[-1]
+  array.pop()
+  array.join(", ") << (", and " + last_index )
+end
 end
