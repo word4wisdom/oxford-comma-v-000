c532024c2b7d9c5086f7b4a147965002a7b49937
Joan' Araujo <wisdombthegame@gmail.com>
2020-08-22 23:12:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,2 @@
-def oxford_comma(array)
-if array.count == 1
-return array.join
-elsif  array.count == 2
-  array.join(" and ")
-else
-  last_index = array[-1]
-  array.pop()
-  array.join(", ") << (", and " + last_index )
-end
+def oxford_comma
 end
