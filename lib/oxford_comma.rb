74359a9f662804ffa221d5477b052364b3781a29
Joan' Araujo <wisdombthegame@gmail.com>
2020-08-22 23:16:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,2 +1,12 @@
-def oxford_comma
-end
+def oxford_comma(array)
+  if array.count == 1
+   array.join
+  elsif array.count == 2
+   array.join("and")
+ else
+   end
+ end
+
+    #adds 'and' between elements when given a 2-element array (FAILED - 2)
+    #adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
+    #correctly formats arrays of lengths greater than three (FAILED - 4)
