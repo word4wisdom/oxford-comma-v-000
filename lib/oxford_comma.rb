d36043336cc9c2ed91dac3b11665593e221fd6cd
Joan' Araujo <wisdombthegame@gmail.com>
2020-08-22 23:24:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,13 @@ def oxford_comma(array)
    array.join
   elsif array.count == 2
    array.join(" and ")
- else
+ elsif array.count == 3
+   array.join.(" , and ")
    end
  end
 
     #adds 'and' between elements when given a 2-element array (FAILED - 2)
     #adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
     #correctly formats arrays of lengths greater than three (FAILED - 4)
+    #adds commas plus a final 'and' when given a 3-element array (FAILED - 1)
+    #correctly formats arrays of lengths greater than three (FAILED - 2)
