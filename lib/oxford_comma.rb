4170fb47b3af2ef3123fa9330bb3831689e79720
Joan' Araujo <wisdombthegame@gmail.com>
2020-08-22 23:44:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.count == 2
    array.join(" and ")
  elsif array.count == 3
-   array.insert(-2, "and").join(", ")
+   array.insert(-2, "and").join
  else
    end
  end
